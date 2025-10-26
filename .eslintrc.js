7e9633db92a35ab04c405a87649a44f18c05194d
webert <webertfernandes15@hotmail.com>
2025-10-26 03:21:39 -0300

    chore: add ESLint configuration and fix code quality issues

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2021: true
+  },
+  extends: [
+    'eslint:recommended',
+    '@vue/eslint-config-typescript/recommended',
+    'plugin:vue/vue3-essential'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/no-unused-vars': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+  },
+  ignorePatterns: ['dist/', 'node_modules/', '*.config.js']
+}
