448988fc5c524b4a8c1b8e0c54e8b3bf72889102
webert <webertfernandes15@hotmail.com>
2025-10-26 03:34:19 -0300

    fix: add SonarJS rules and fix code quality issues

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,13 +9,14 @@ module.exports = {
     'eslint:recommended',
     '@vue/eslint-config-typescript/recommended',
     'plugin:vue/vue3-essential',
-    'plugin:security/recommended'
+    'plugin:security/recommended',
+    'plugin:sonarjs/recommended'
   ],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module'
   },
-  plugins: ['security'],
+  plugins: ['security', 'sonarjs'],
   rules: {
     'vue/multi-word-component-names': 'off',
     'vue/no-unused-vars': 'error',
@@ -24,7 +25,10 @@ module.exports = {
     'no-debugger': 'error',
     'security/detect-object-injection': 'off',
     'security/detect-non-literal-regexp': 'off',
-    'security/detect-unsafe-regex': 'off'
+    'security/detect-unsafe-regex': 'off',
+    'sonarjs/cognitive-complexity': ['error', 15],
+    'sonarjs/no-duplicate-string': ['error', 5],
+    'sonarjs/no-identical-functions': 'error'
   },
   ignorePatterns: ['dist/', 'node_modules/', '*.config.js']
 }
