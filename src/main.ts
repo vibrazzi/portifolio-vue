90fe04c37ebcbc329c974f0f9afad06e7a167311
webert <webertfernandes15@hotmail.com>
2025-12-16 11:27:17 -0300

    refactor: update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 
 import './style.css'
 
+// @ts-ignore: Could not find module './App.vue' or its type declarations
 import App from './App.vue'
 
 createApp(App).mount('#app')
