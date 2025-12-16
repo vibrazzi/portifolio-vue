33ddf6971f1576ead06dec224b4012550e8f3c5b
webert <webertfernandes15@hotmail.com>
2025-12-16 11:27:23 -0300

    refactor: clean up vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,31 +5,23 @@ import tailwindcss from 'tailwindcss';
 
 export default defineConfig({
   plugins: [
-    vue(), // Plugin para suporte ao Vue.js
+    vue(),
   ],
   css: {
     postcss: {
       plugins: [
-        tailwindcss(), // Suporte ao Tailwind CSS
+        tailwindcss(),
       ],
     },
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)), // Alias para facilitar importações
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
   server: {
-    host: '0.0.0.0', // Permite acesso externo ao servidor de desenvolvimento
-    port: 5173, // Porta padrão do Vite
-    open: true, // Abre o navegador automaticamente ao iniciar o servidor
-    proxy: {
-      '/api': {
-        target: process.env.VITE_BACKEND_URL || 'http://localhost:3000', // Proxy para o backend
-        changeOrigin: true, // Permite redirecionar para origens diferentes
-        secure: false, // Permite conexões HTTP não seguras (útil para desenvolvimento)
-        rewrite: (path) => path.replace(/^\/api/, ''), // Remove o prefixo "/api" ao encaminhar
-      },
-    },
+    host: '0.0.0.0',
+    port: 5173,
+    open: true,
   },
 });
